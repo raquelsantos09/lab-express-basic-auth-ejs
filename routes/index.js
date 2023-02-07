dfc6a2e1c23cdba96ffba45aad1b33fd9ad50dfb
Raquel Santos <raquelmrsantos@gmail.com>
2023-02-07 15:08:20 +0000

    added routes and views

--- routes/index.js
+++ routes/index.js
@@ -7,22 +7,6 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-router.get("/signup", (req, res, next) => {
-  res.render("signup");
-});
-
-
-router.post('/signup', async (req, res, next) => {
-console.log(req.body)
-const body  = {...req.body}
-const salt = bcrypt.genSaltSync(13)
-const passswordHash = bcrypt.hashSync(body.password, salt)
-console.log(passswordHash)
-delete body.password
-body.passswordHash = passswordHash
-await User.create(body)
-res.send(body)
-})
 
 
 module.exports = router;
